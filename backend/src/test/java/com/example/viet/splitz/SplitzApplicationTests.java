4f3c694acfc1aca97b12d4b57b3be7ff1d203e1e
VietCT <transonviet2004@gmail.com>
2025-10-06 21:52:08 +0800

    fix: fixed ci/cd mvn run test error

--- backend/src/test/java/com/example/viet/splitz/SplitzApplicationTests.java
+++ backend/src/test/java/com/example/viet/splitz/SplitzApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.viet.splitz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class SplitzApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
